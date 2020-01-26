b4ced5fb1541ab9dfd6556a4fa1babe37e83addc
jauney <47222003+jauney@users.noreply.github.com>
2020-01-26 11:59:44 +0800

    add print

--- src/utils/print.js
+++ src/utils/print.js
@@ -1,13 +1,59 @@
-import getLodop from './LodopFuncs';
 
-export function print({ width = 800, height = 2000, html = '' }) {
-  let LODOP = getLodop();
-  LODOP.PRINT_INIT("托运单");
-  LODOP.SET_PRINT_PAGESIZE(1, width, height, '')
-  //LODOP.SET_PRINT_STYLE("FontSize", 18);
-  //LODOP.SET_PRINT_STYLE("Bold", 1);
-  // LODOP.ADD_PRINT_TEXT(50, 231, 500, 39, "打印页面部分内容");
-  LODOP.ADD_PRINT_HTM(0, 0, '100%', height, html);
-  //参数按顺序分别为，上下间距，左右间距，宽度，高度
-  LODOP.PRINT();
+import moment from 'moment';
+
+export function print({ selectedRows = [], type = '' }) {
+  let bodyHTML = ''
+  selectedRows.forEach(item => {
+    let transType = '提付'
+    if (item.trans_type == 1) {
+      transType = '回付'
+    }
+    else if (item.trans_type == 2) {
+      transType = '现付'
+    }
+    bodyHTML += `<tr>
+        <td>${item.order_code || ''}</td>
+        <td>${item.sendcustomer_name || ''}</td>
+        <td>${item.getcustomer_name || ''}</td>
+        <td>${item.order_real || ''}</td>
+        <td>${item.trans_discount || ''}</td>
+        <td>${transType || ''}</td>
+        <td>${item.order_advancepay_amount || ''}</td>
+        <td>${item.insurance_fee || ''}</td>
+        <td>${item.order_name || ''}</td>
+        <td>${ item.depart_date ? moment(Number(item.depart_date || 0)).format('YYYY-MM-DD HH:mm:ss') : ''}</td>
+        <td>${item.site_name || ''}</td>
+        </tr>`
+  })
+  let styles = `
+    <style>
+    .content, .header {text-align: center; padding: 10px 0 20px;}
+    table {width: 100%; border-collapse: collapse; border-spacing: 0;}
+    table th { font-weight: bold; }
+    table th, table td {border: 1px solid #ccc; font-size: 10px; padding: 4px; text-align: left; line-height: 150%;}
+    </style>`
+  let html = `
+    <div class="header">陕西远诚宝路通物流</div>
+    <div class="content">
+    <table>
+      <tr>
+        <th style="width:50px;">货单号</th>
+        <th style="width:50px;">发货客户</th>
+        <th style="width:50px;">收货客户</th>
+        <th style="width:50px;">实收货款</th>
+        <th style="width:50px;">折后运费</th>
+        <th style="width:50px;">运费方式</th>
+        <th style="width:50px;">垫付</th>
+        <th style="width:50px;">保价费</th>
+        <th style="width:120px;">货物名称</th>
+        <th style="width:110px;">发车时间</th>
+        <th style="width:50px;">站点</th>
+      </tr>
+      ${bodyHTML}
+    </table>
+    </div>
+    `
+  //告诉渲染进程，开始渲染打印内容
+  const printOrderWebview = document.querySelector('#printWebview')
+  printOrderWebview.send('webview-print-render', { printHtml: `${styles}${html}`, type })
 }
