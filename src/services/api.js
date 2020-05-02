ede7de6cdbd1d1cdfd22a9cb299215e79a6fc4d2
chenping.06 <chenping.06@bytedance.com>
2020-05-02 21:28:04 +0800

    feat: no cache

--- src/services/api.js
+++ src/services/api.js
@@ -24,11 +24,21 @@ const httpLink = new HttpLink({
     authorization: localStorage.getItem('token') || '',
   }
 });
-
+const defaultOptions = {
+  watchQuery: {
+    fetchPolicy: 'no-cache',
+    errorPolicy: 'ignore',
+  },
+  query: {
+    fetchPolicy: 'no-cache',
+    errorPolicy: 'all',
+  },
+}
 const cache = new InMemoryCache();
 const client = new ApolloClient({
   link: httpLink,
-  cache
+  cache,
+  defaultOptions: defaultOptions
 });
 
 /**
